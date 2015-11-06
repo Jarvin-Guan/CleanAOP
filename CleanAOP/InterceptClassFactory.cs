b6771be9863128c4ec400a6c2355765187b8a7b2
jarvin_guan <jarvin_g@126.com>
2015-11-07 00:51:36 +0800

    修改代理生成方式

--- CleanAOP/InterceptClassFactory.cs
+++ CleanAOP/InterceptClassFactory.cs
@@ -13,29 +13,18 @@ namespace CleanAOP
     /// </summary>
     public class InterceptClassFactory
     {
-        private static Dictionary<IntercetEnum, IInterceptor> intercepDic = new Dictionary<IntercetEnum, IInterceptor>();
-        static InterceptClassFactory()
-        {
-            intercepDic.Add(IntercetEnum.PropertyNotify, new PropertyNotifyIntercept());
-            intercepDic.Add(IntercetEnum.Aop, new AopIntercept());
-        }
         /// <summary>
         /// 获取方法拦截代理类实例
         /// </summary>
         /// <typeparam name="T">被代理类</typeparam>
         /// <param name="interceptors">拦截器列表</param>
         /// <returns></returns>
-        public static T GetInterceptClass<T>(object[] args,params IntercetEnum[] interceptEnums) where T : class
+        public static T GetInterceptClass<T>(params object[] args) where T : class
         {
             ProxyGenerator generator = new ProxyGenerator();//实例化【代理类生成器】
 
-            List<IInterceptor> intercepts = new List<IInterceptor>();
-            foreach(var interceptenum in interceptEnums)
-            {
-                intercepts.Add(intercepDic[interceptenum]);
-            };
-
-            T result = generator.CreateClassProxy(typeof(T), args, intercepts.ToArray()) as T;//创建方法拦截代理类实例。
+            T result = generator.CreateClassProxy(typeof(T), args, 
+                typeof(T).GetCustomAttributes(typeof(IInterceptor), true).Cast<IInterceptor>().ToArray()) as T;//创建方法拦截代理类实例。
             return result;
         }
     }
